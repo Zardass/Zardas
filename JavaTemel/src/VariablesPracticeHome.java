ebcf5830a8f066c4e3643d408a6db9db9688d1e8
Zardass <salimzarda87@gmail.com>
2024-04-23 19:51:24 +0400

    int

---
+++ JavaTemel/src/VariablesPracticeHome.java
@@ -0,0 +1,11 @@
+public class VariablesPracticeHome {
+    public static void main(String[] args) {
+        int age = 25;
+        System.out.println("age : " + age);
+        //*********************************
+
+        
+
+
+    }
+}
