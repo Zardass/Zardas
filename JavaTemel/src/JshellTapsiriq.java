8b3c4a5539aaba51bbe5a1699f8ad6a4ae6d4af8
Zardass <salimzarda87@gmail.com>
2024-04-24 21:36:32 +0400

    jhell uzerinde emeller

---
+++ JavaTemel/src/JshellTapsiriq.java
@@ -0,0 +1,45 @@
+public class JshellTapsiriq {
+    public static void main(String[] args) {
+       /* C:\Users\HP>jshell
+                |  Welcome to JShell -- Version 21.0.3
+                |  For an introduction type: /help intro
+
+        jshell> int age = 7
+        age ==> 7
+
+        jshell> age = 14
+        age ==> 14
+
+        jshell> byte b = 7
+        b ==> 7
+
+        jshell> short s = 7
+        s ==> 7
+
+        jshell> long l = 7
+        l ==> 7
+
+        jshell> double d = 7.9d
+        d ==> 7.9
+
+        jshell> float f = 7.9f
+        f ==> 7.9
+
+        jshell> char c = 'A'
+        c ==> 'A'
+
+        jshell> System.out.println("int\nshort\nbyte\ndouble\nvoid\nstatic\nlong\nfloat\nprivate");
+        int
+        short
+        byte
+        double
+        void
+        static
+        long
+        float
+        private
+        
+        */
+
+    }
+}
