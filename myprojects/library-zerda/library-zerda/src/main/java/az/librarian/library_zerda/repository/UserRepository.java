28c6196310336d8117d8f9d778763b09556b74d7
Zardass <salimzarda87@gmail.com>
2024-09-23 00:43:29 +0400

    user repository

---
+++ myprojects/library-zerda/library-zerda/src/main/java/az/librarian/library_zerda/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package az.librarian.library_zerda.repository;
+
+import az.librarian.library_zerda.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
+
+}
