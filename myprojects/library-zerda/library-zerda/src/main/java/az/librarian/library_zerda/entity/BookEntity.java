9f140e117cbfbe13aba2f25944c567e6d8d8a81e
Zardass <salimzarda87@gmail.com>
2024-09-22 19:36:54 +0400

    BookEntity is created

---
+++ myprojects/library-zerda/library-zerda/src/main/java/az/librarian/library_zerda/entity/BookEntity.java
@@ -0,0 +1,28 @@
+package az.librarian.library_zerda.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class BookEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String publishDate;
+
+    private String author;
+
+    private String pageCount;
+
+
+}
